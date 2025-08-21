ab61f67adaae4fc09f66a7df9670f862e474c734
HuwBYB <huw@chhlogistics.co.uk>
2025-08-21 20:45:12 +0100

    Update ConfidenceScreen.tsx

--- src/ConfidenceScreen.tsx
+++ src/ConfidenceScreen.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState, type ReactNode } from "react";
+import { useEffect, useRef, useState, type ReactNode } from "react";
 import { supabase } from "./lib/supabaseClient";
 
 /* ---------- tiny helpers ---------- */
